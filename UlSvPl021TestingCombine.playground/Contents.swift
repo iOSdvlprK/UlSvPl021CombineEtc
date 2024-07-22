1712aa543c27f6e0115d6edcf51f10b14088044c
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-07-23 00:10:50 +0900

    test combine code

---
+++ UlSvPl021TestingCombine.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+import Combine
+import XCTest
+
+class HelloCombineTests: XCTestCase {
+    func testFirstTest() {
+        let expectation = XCTestExpectation(description: "Received value")
+        let publisher = Just("Hello World!")
+        
+        let _ = publisher.sink { value in
+            XCTAssertEqual(value, "Hello World!")
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+}
+
+HelloCombineTests.defaultTestSuite.run()
+
+
+
+
